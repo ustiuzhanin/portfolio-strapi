3bc908d2e9da52232b35e71693b5430eaef2a1db
Slava <ucheba00@gmail.com>
2021-04-11 11:52:03 -0600

    init strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0313546503a151289728321acf21e00'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b9fe40b-600e-448f-b70f-996f756cbfc6'
+};
